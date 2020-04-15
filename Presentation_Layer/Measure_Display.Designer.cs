d6d5d4a10a315076a8b12179fa91bf12411b29b9
Ina Meyer <201808400@post.au.dk>
2020-04-15 12:54:57 +0200

    Brugergrænseflade ændringer

--- Presentation_Layer/Measure_Display.Designer.cs
+++ Presentation_Layer/Measure_Display.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Regret_ECGB = new System.Windows.Forms.Button();
+            this.Start_SendB = new System.Windows.Forms.Button();
+            this.MeasureNumberTB = new System.Windows.Forms.TextBox();
+            this.NameL = new System.Windows.Forms.Label();
+            this.NameMeasureTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Regret_ECGB
+            // 
+            this.Regret_ECGB.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Regret_ECGB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Regret_ECGB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Regret_ECGB.Location = new System.Drawing.Point(32, 371);
+            this.Regret_ECGB.Name = "Regret_ECGB";
+            this.Regret_ECGB.Size = new System.Drawing.Size(189, 47);
+            this.Regret_ECGB.TabIndex = 0;
+            this.Regret_ECGB.Text = "Ny patient";
+            this.Regret_ECGB.UseVisualStyleBackColor = false;
+            // 
+            // Start_SendB
+            // 
+            this.Start_SendB.BackColor = System.Drawing.Color.Green;
+            this.Start_SendB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Start_SendB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Start_SendB.Location = new System.Drawing.Point(707, 360);
+            this.Start_SendB.Name = "Start_SendB";
+            this.Start_SendB.Size = new System.Drawing.Size(224, 69);
+            this.Start_SendB.TabIndex = 1;
+            this.Start_SendB.Text = "Start EKG-måling";
+            this.Start_SendB.UseVisualStyleBackColor = false;
+            // 
+            // MeasureNumberTB
+            // 
+            this.MeasureNumberTB.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.MeasureNumberTB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MeasureNumberTB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MeasureNumberTB.Location = new System.Drawing.Point(617, 30);
+            this.MeasureNumberTB.Name = "MeasureNumberTB";
+            this.MeasureNumberTB.Size = new System.Drawing.Size(69, 34);
+            this.MeasureNumberTB.TabIndex = 2;
+            this.MeasureNumberTB.Visible = false;
+            // 
+            // NameL
+            // 
+            this.NameL.AutoSize = true;
+            this.NameL.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.NameL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NameL.Location = new System.Drawing.Point(13, 27);
+            this.NameL.Name = "NameL";
+            this.NameL.Size = new System.Drawing.Size(65, 27);
+            this.NameL.TabIndex = 3;
+            this.NameL.Text = "Navn:";
+            this.NameL.Visible = false;
+            // 
+            // NameMeasureTB
+            // 
+            this.NameMeasureTB.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.NameMeasureTB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.NameMeasureTB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NameMeasureTB.Location = new System.Drawing.Point(84, 30);
+            this.NameMeasureTB.Name = "NameMeasureTB";
+            this.NameMeasureTB.Size = new System.Drawing.Size(273, 34);
+            this.NameMeasureTB.TabIndex = 4;
+            this.NameMeasureTB.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(526, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Måling: ";
+            this.label1.Visible = false;
+            // 
+            // Measure_Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(972, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NameMeasureTB);
+            this.Controls.Add(this.NameL);
+            this.Controls.Add(this.MeasureNumberTB);
+            this.Controls.Add(this.Start_SendB);
+            this.Controls.Add(this.Regret_ECGB);
+            this.Name = "Measure_Display";
             this.Text = "Measure_Display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Regret_ECGB;
+        private System.Windows.Forms.Button Start_SendB;
+        private System.Windows.Forms.TextBox MeasureNumberTB;
+        private System.Windows.Forms.Label NameL;
+        private System.Windows.Forms.TextBox NameMeasureTB;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Presentation_Layer/Patient_Display.Designer.cs
+++ Presentation_Layer/Patient_Display.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CPRTB = new System.Windows.Forms.TextBox();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.AdressTB = new System.Windows.Forms.TextBox();
+            this.SaveB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(287, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Indtast patientdata";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(61, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPR-nummer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(61, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Navn:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(61, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adresse:";
+            // 
+            // CPRTB
+            // 
+            this.CPRTB.Location = new System.Drawing.Point(219, 130);
+            this.CPRTB.Name = "CPRTB";
+            this.CPRTB.Size = new System.Drawing.Size(375, 31);
+            this.CPRTB.TabIndex = 4;
+            // 
+            // NameTB
+            // 
+            this.NameTB.Location = new System.Drawing.Point(219, 204);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(375, 31);
+            this.NameTB.TabIndex = 5;
+            // 
+            // AdressTB
+            // 
+            this.AdressTB.Location = new System.Drawing.Point(219, 273);
+            this.AdressTB.Name = "AdressTB";
+            this.AdressTB.Size = new System.Drawing.Size(375, 31);
+            this.AdressTB.TabIndex = 6;
+            // 
+            // SaveB
+            // 
+            this.SaveB.BackColor = System.Drawing.Color.Green;
+            this.SaveB.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SaveB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SaveB.Location = new System.Drawing.Point(276, 351);
+            this.SaveB.Name = "SaveB";
+            this.SaveB.Size = new System.Drawing.Size(251, 59);
+            this.SaveB.TabIndex = 7;
+            this.SaveB.Text = "Gem patientoplysninger";
+            this.SaveB.UseVisualStyleBackColor = false;
+            // 
+            // Patient_Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveB);
+            this.Controls.Add(this.AdressTB);
+            this.Controls.Add(this.NameTB);
+            this.Controls.Add(this.CPRTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Patient_Display";
             this.Text = "Patient_Display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox CPRTB;
+        private System.Windows.Forms.TextBox NameTB;
+        private System.Windows.Forms.TextBox AdressTB;
+        private System.Windows.Forms.Button SaveB;
     }
 }
