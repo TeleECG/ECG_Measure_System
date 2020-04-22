5f40b6f01c468c2bfa7a3a23ddcc4326efcda9c7
carolinenissen <201806156@uni.au.dk>
2020-04-22 09:57:46 +0200

    ....

--- Presentation_Layer/Program.cs
+++ Presentation_Layer/Program.cs
@@ -17,7 +17,7 @@ namespace Presentation_Layer
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form());
         }
     }
 }
