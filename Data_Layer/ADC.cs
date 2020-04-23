d11b3ad63290598276f22c75ffa8c8fcca2440f1
Ina Meyer <201808400@post.au.dk>
2020-04-23 08:56:53 +0200

    Hente csv fil ind

--- Data_Layer/ADC.cs
+++ Data_Layer/ADC.cs
@@ -1,13 +1,37 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Text;
+using LumenWorks.Framework.IO.Csv;
 
 namespace Data_Layer
 {
     public class ADC : IADC
     {
         //Indlæs signal fra Physionet
+        public void ReadCsv()
+        {
+            //1. Lead
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Users\ina-m\OneDrive\Dokumenter\4. semester\Projekt\EKG-signaler\1Lead.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
 
+            //2. Lead
+            var csvTable2 = new DataTable();
+            using (var csvReader2 = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Users\ina-m\OneDrive\Dokumenter\4. semester\Projekt\EKG-signaler\2Lead.csv")), true))
+            {
+                csvTable2.Load(csvReader2);
+            }
+
+            var csvTable3 = new DataTable();
+            using (var csvReader3 = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Users\ina-m\OneDrive\Dokumenter\4. semester\Projekt\EKG-signaler\3Lead.csv")), true))
+            {
+                csvTable3.Load(csvReader3);
+            }
+        }
     }
 }
--- Presentation_Layer/Program.cs
+++ Presentation_Layer/Program.cs
@@ -1,3 +1,4 @@
+using Logic_Layer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,18 +7,27 @@ using System.Windows.Forms;
 
 namespace Presentation_Layer
 {
-    static class Program
+    public class Program //Der stod static først
     {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+        Enter_Patient_Data_Controller _patient_Data_Controller;
+        Measure_Display _measure_Display;
+        public Program(Enter_Patient_Data_Controller patient_Data_Controller, Measure_Display measure_Display )
+        {
+            _patient_Data_Controller = patient_Data_Controller;
+            _measure_Display = measure_Display;
+        }
         [STAThread]
-        static void Main()
+       
+        public override void Main()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new Patient_Display(_patient_Data_Controller, _measure_Display));
         }
     }
 }
