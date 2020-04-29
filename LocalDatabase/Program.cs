ca4b7223d122a8c92ce5e88c19c853b51b6b0a39
Josefine Rosenskjold Madsen <au602882@uni.au.dk>
2020-04-29 22:36:39 +0200

    Tilføjelse af noter til Lokal database

--- LocalDatabase/Program.cs
+++ LocalDatabase/Program.cs
@@ -7,55 +7,27 @@ namespace LocalDatabase
     {
         static void Main()
         {
-            //using (var db = new BloggingContext())
-            //{
-            //    // Create
-            //    Console.WriteLine("Inserting a new blog");
-            //    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-            //    db.SaveChanges();
-
-            //    // Read
-            //    Console.WriteLine("Querying for a blog");
-            //    var blog = db.Blogs
-            //        .OrderBy(b => b.BlogId)
-            //        .First();
-
-            //    // Update
-            //    Console.WriteLine("Updating the blog and adding a post");
-            //    blog.Url = "https://devblogs.microsoft.com/dotnet";
-            //    blog.Posts.Add(
-            //        new Post
-            //        {
-            //            Title = "Hello World",
-            //            Content = "I wrote an app using EF Core!"
-            //        });
-            //    db.SaveChanges();
-
-            //    // Delete
-            //    Console.WriteLine("Delete the blog");
-            //    db.Remove(blog);
-            //    db.SaveChanges();
-            //}
-            using (var db = new BloggingContext())
+            using (var db = new BloggingContext()) //Opretter objekt af klassen BloggingContext
             {
                 // Create
                 Console.WriteLine("Inserting a new blog");
 
+                //Opretter objekt af patient og tilføjer de tilhørende attributter (Disse skal evt. hentes fra model-klassen i logic layer 
                 var Patient = new Patient() { CPRNumber = "123456-1234", Name = "Inger", Pulse = 65 };
-                var ECGM = new ECGMeasurement();
-                var ECGLead = new ECGLead();
-                var ECGLead1 = new ECGLead();
-                var ECGLead2 = new ECGLead();
-                ECGM.Title = "Test";
-                ECGLead.LeadNumber = 1;
-                ECGLead1.LeadNumber = 2;
-                ECGLead2.LeadNumber = 3;
-                ECGM.ECGLeads.Add(ECGLead);
-                ECGM.ECGLeads.Add(ECGLead1);
-                ECGM.ECGLeads.Add(ECGLead2);
-                Patient.ECGMeasurements.Add(ECGM);
-                db.Add(Patient);
-                db.SaveChanges();
+                var ECGM = new ECGMeasurement(); //Opretter objekt af ECGMeasuremnt
+                var ECGLead = new ECGLead(); //Opretter objekt af ECGLead
+                var ECGLead1 = new ECGLead(); //Opretter nyt objekt af ECGLead
+                var ECGLead2 = new ECGLead(); //Opretter endnu et nyt objekt af ECGLead
+                ECGM.Title = "Test"; // Sætter målingens "Title" til noget - eks. målenummer
+                ECGLead.LeadNumber = 1; //Sætter hvilket lead der er målt
+                ECGLead1.LeadNumber = 2; //Sætter hvilket lead der er målt
+                ECGLead2.LeadNumber = 3; //Sætter hvilket lead der er målt
+                ECGM.ECGLeads.Add(ECGLead); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
+                ECGM.ECGLeads.Add(ECGLead1); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
+                ECGM.ECGLeads.Add(ECGLead2); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
+                Patient.ECGMeasurements.Add(ECGM); // Her tilføjes ECGMeasurement objektet til listen af ECGMeasurement i Patient
+                db.Add(Patient); //Her tilføjes Patient objektet til BloggingContext
+                db.SaveChanges(); //Det gemmes
 
 
                 // Read
--- LocalDatabase/localDbModel.cs
+++ LocalDatabase/localDbModel.cs
@@ -9,43 +9,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LocalDatabase
 {
-    //public class BloggingContext : DbContext
-    //{
-    //    public DbSet<Blog> Blogs { get; set; }
-    //    public DbSet<Post> Posts { get; set; }
-
-    //    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //        => options.UseSqlite("Data Source=blogging.db");
-    //}
-
-    //public class Blog
-    //{
-    //    public int BlogId { get; set; }
-    //    public string Url { get; set; }
-
-    //    public List<Post> Posts { get; } = new List<Post>();
-    //}
-
-    //public class Post
-    //{
-    //    public int PostId { get; set; }
-    //    public string Title { get; set; }
-    //    public string Content { get; set; }
-
-    //    public int BlogId { get; set; }
-    //    public Blog Blog { get; set; }
-    //}
-    public class BloggingContext : DbContext
+    public class BloggingContext : DbContext //Omdøb eventuelt klasse til "PatientContext"
     {
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<ECGMeasurement> ECGMeasurements { get; set; }
-        public DbSet<ECGLead> ECGLeads { get; set; }
+        public DbSet<Patient> Patients { get; set; } // Hvad bruges dette til? Bruger man den til at kalde eller?
+        public DbSet<ECGMeasurement> ECGMeasurements { get; set; } // Hvad bruges dette til? Bruger man den til at kalde eller?
+        public DbSet<ECGLead> ECGLeads { get; set; } // Hvad bruges dette til? Bruger man den til at kalde eller?
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=LocalPatientDbV1.db");
+            => options.UseSqlite("Data Source=LocalPatientDbV1.db"); // Hvad sker der her? 
     }
 
-    public class Patient // Patientklassen sendes i databasen med følgende: 
+    public class Patient // Patient klassen sendes i databasen med følgende: 
     {
         public int PatientId { get; set; } //Et autogenerede Id-nummer, der kun passer til den specifikke patient. 
         public string CPRNumber { get; set; } // CPR-nummer tilhørende patienten, som indtastes på brugergrænsefladen.
@@ -57,24 +31,24 @@ namespace LocalDatabase
         public List<ECGMeasurement> ECGMeasurements { get; } = new List<ECGMeasurement>(); // En liste bestående af objekter af klassen ECGMeasurement.
     }
 
-    public class ECGMeasurement
+    public class ECGMeasurement // ECGMeasurement klassen sendes i databasen og kobles op med Patientobjektet med følgende: 
     {
-        public int ECGMeasurementId { get; set; }
-        //public List<int> Values { get; set; } = new List<int>();
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public int PatientId { get; set; }
-        public Patient Patient { get; set; }
-        public List<ECGLead> ECGLeads { get; set; } = new List<ECGLead>();
+        public int ECGMeasurementId { get; set; } //Et autogenerede Id-nummer, der kun passer til den specifikke ECGMeasurement. 
+        public string Title { get; set; } //Slettes!
+        public string Content { get; set; } //Slettes!
+        //public string MeasurementNumber { get; set; } // Et nummer der afgør om det er 1., 2., eller 3. måling
+        public int PatientId { get; set; } //Tilknytter ECGMeasurement til den specifikke Patient via PatientId
+        public Patient Patient { get; set; } //Objekt af Patient klassen 
+        public List<ECGLead> ECGLeads { get; set; } = new List<ECGLead>(); // En liste bestående af objekter af klassen ECGLead.
     }
-    public class ECGLead
+    public class ECGLead // ECGMeasurement klassen sendes i databasen og kobles op med ECGMeasurementobjektet med følgende: 
     {
-        public int ECGLeadId { get; set; }
-        public ECGMeasurement ECGMeasurement { get; set; }
-        public int ECGMeasurementId { get; set; }
-        public int LeadNumber { get; set; }
-        public int ECGLeadValue { get; set; }
-        //public List<double> ECGLeadValues { get; set; } = new List<double>();
+        public int ECGLeadId { get; set; } //Et autogenerede Id-nummer, der kun passer til det specifikke ECGLead. 
+        public ECGMeasurement ECGMeasurement { get; set; } //Objekt af ECGMeasurement klassen 
+        public int ECGMeasurementId { get; set; } //Tilknytter ECGMeasurement til den specifikke ECGMeasurement via ECGMeasurementId
+        public int LeadNumber { get; set; } // Et nummer der afgør om det er 1., 2., eller 3. afledning der i dette lead er målt
+        public int ECGLeadValue { get; set; } // Midlertidigt tal der skal repræsentere listen af EKG-værdier, som ses nedenfor. 
+        //public List<double> ECGLeadValues { get; set; } = new List<double>(); // En liste bestående af tal der repræsenterer EKG-målingen 
 
     }
 
