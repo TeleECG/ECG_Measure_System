058378f68fcc4965a8a432883f26cf810baa4946
Josefine Rosenskjold Madsen <au602882@uni.au.dk>
2020-05-04 16:49:32 +0200

    Endelige ændringer til lokal database

--- LocalDatabase/Program.cs
+++ LocalDatabase/Program.cs
@@ -13,42 +13,18 @@ namespace LocalDatabase
             {
                 // Create
                 Console.WriteLine("Inserting a new blog");
-                //Opretter objekt af patient og tilføjer de tilhørende attributter (Disse skal evt. hentes fra model-klassen i logic layer 
-                var PatientMeasurement = new PatientMeasurement() { CPRNumber = _model._CPRNumber, Name = _model._Name, Address=_model._Address, Pulse=_model._Pulse, HRV=_model._HRV };
-                var ECGMeasurement1 = new ECGMeasurement(); //Opretter objekt af ECGMeasuremnt
-                var ECGMeasurement2 = new ECGMeasurement(); //Opretter objekt af ECGMeasuremnt
-                var ECGMeasurement3 = new ECGMeasurement(); //Opretter objekt af ECGMeasuremnt
-                var ECGLead1_1 = new ECGLead() {ECGLeadValues=_model.ECGLeadValues }; //Opretter objekt af ECGLead
-                var ECGLead2_1 = new ECGLead(); //Opretter nyt objekt af ECGLead
-                var ECGLead3_1 = new ECGLead(); //Opretter endnu et nyt objekt af ECGLead
-                var ECGLead1_2 = new ECGLead(); //Opretter objekt af ECGLead
-                var ECGLead2_2 = new ECGLead(); //Opretter nyt objekt af ECGLead
-                var ECGLead3_2 = new ECGLead(); //Opretter endnu et nyt objekt af ECGLead
-                var ECGLead1_3 = new ECGLead(); //Opretter objekt af ECGLead
-                var ECGLead2_3 = new ECGLead(); //Opretter nyt objekt af ECGLead
-                var ECGLead3_3 = new ECGLead(); //Opretter endnu et nyt objekt af ECGLead
-                ECGLead1_1.LeadNumber = 1; //Sætter hvilket lead der er målt
-                ECGLead2_1.LeadNumber = 2; //Sætter hvilket lead der er målt
-                ECGLead3_1.LeadNumber = 3; //Sætter hvilket lead der er målt
-                ECGLead1_2.LeadNumber = 1; //Sætter hvilket lead der er målt
-                ECGLead2_2.LeadNumber = 2; //Sætter hvilket lead der er målt
-                ECGLead3_2.LeadNumber = 3; //Sætter hvilket lead der er målt
-                ECGLead1_3.LeadNumber = 1; //Sætter hvilket lead der er målt
-                ECGLead2_3.LeadNumber = 2; //Sætter hvilket lead der er målt
-                ECGLead3_3.LeadNumber = 3; //Sætter hvilket lead der er målt
-                ECGMeasurement1.ECGLeads.Add(ECGLead1_1); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement1.ECGLeads.Add(ECGLead2_1); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement1.ECGLeads.Add(ECGLead3_1); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement2.ECGLeads.Add(ECGLead1_2); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement2.ECGLeads.Add(ECGLead2_2); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement2.ECGLeads.Add(ECGLead3_2); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement3.ECGLeads.Add(ECGLead1_3); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement3.ECGLeads.Add(ECGLead2_3); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-                ECGMeasurement3.ECGLeads.Add(ECGLead3_3); // Her tilføjes ECGLead objektet til listen af ECGLead i ECGMeasurement
-
-                PatientMeasurement.ECGMeasurements.Add(ECGMeasurement1); // Her tilføjes ECGMeasurement objektet til listen af ECGMeasurement i Patient
-                PatientMeasurement.ECGMeasurements.Add(ECGMeasurement2); // Her tilføjes ECGMeasurement objektet til listen af ECGMeasurement i Patient
-                PatientMeasurement.ECGMeasurements.Add(ECGMeasurement3); // Her tilføjes ECGMeasurement objektet til listen af ECGMeasurement i Patient
+                //Opretter objekt af patient og tilføjer de tilhørende attributter
+                var PatientMeasurement = new PatientMeasurement(_model._CPRNumber, _model._Name, _model._Address);
+                
+                PatientMeasurement.ECGMeasurements[0].ECGLeads[0].ECGLeadValues = _model.ECGLeadValues1_1; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[0].ECGLeads[1].ECGLeadValues = _model.ECGLeadValues1_2; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[0].ECGLeads[2].ECGLeadValues = _model.ECGLeadValues1_3; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[1].ECGLeads[0].ECGLeadValues = _model.ECGLeadValues2_1; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[1].ECGLeads[1].ECGLeadValues = _model.ECGLeadValues2_2; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[1].ECGLeads[2].ECGLeadValues = _model.ECGLeadValues2_3; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[2].ECGLeads[0].ECGLeadValues = _model.ECGLeadValues3_1; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[2].ECGLeads[1].ECGLeadValues = _model.ECGLeadValues3_2; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
+                PatientMeasurement.ECGMeasurements[2].ECGLeads[2].ECGLeadValues = _model.ECGLeadValues3_3; // List af double skal konverteres til byte (Listen af double, da vi konverterede csv-filen)
 
                 db.PatientMeasurements.Add(PatientMeasurement); //Her tilføjes Patient objektet til BloggingContext
                 db.SaveChanges(); //Det gemmes
--- Logic_Layer/Model.cs
+++ Logic_Layer/Model.cs
@@ -15,15 +15,18 @@ namespace Logic_Layer
 
         public DateTime _Date { get; set; }
 
-        public List<List<byte[]>> _ECGMeasurements { get; set; }
-
-        public byte[] ECGLeadValues { get; set; } //Byte-array hvor ECG-værdier skal lægges i
-
-
         public int _Pulse { get; set; }
 
         public int _HRV { get; set; }
 
-       
+        public byte[] ECGLeadValues1_1 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues1_2 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues1_3 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues2_1 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues2_2 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues2_3 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues3_1 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues3_2 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
+        public byte[] ECGLeadValues3_3 { get; set; } //Byte-array hvor ECG-værdier skal lægges i
     }
 }
