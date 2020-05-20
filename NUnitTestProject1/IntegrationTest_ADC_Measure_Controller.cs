6c067ccbe11e75d0e128b310809b728d489904dc
Mie Tollund Sørensen <au586191@uni.au.dk>
2020-05-20 14:12:46 +0200

    Integrationstest for model og controllere

---
+++ NUnitTestProject1/IntegrationTest_ADC_Measure_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Logic_Layer;
+using Data_Layer;
+using NSubstitute;
+using Domain;
+using NSubstitute.ReceivedExtensions;
+using System.Net.Http;
+using Microsoft.VisualBasic.CompilerServices;
+using NUnit.Framework.Constraints;
+
+namespace Test_ECG_Measure_System
+{
+    [TestFixture]
+    class IntegrationTest_ADC_Measure_Controller
+    {
+        private Measure_ECG_Controller _measure_ECG_Controller;
+        private Model _model;
+        private ICalculator _HRV;
+        private ICalculator _puls;
+        private R_peak _rPeak;
+        private IADC _sut;//System Under Test
+
+        [SetUp]
+        public void Setup()
+        {
+            _model = new Model();
+            _HRV = Substitute.For<HRV_Calculator>();
+            _puls = Substitute.For<Pulse_Calculator>();
+            _rPeak = Substitute.For<R_peak>();
+            _sut = new ADC();
+            _measure_ECG_Controller = new Measure_ECG_Controller(_model,_sut,_HRV,_puls,_rPeak);
+
+        }
+
+       
+        [Test]
+        public void Test_ADC_MeasureECGController_MeasureECG_ReadCSVLead1()
+        {
+            int measureNumber = 1;
+            _measure_ECG_Controller.Measure_ECG(measureNumber);
+            //Assert.That(_sut.Received(1).ReadCsvLead1(),);
+          
+           
+            
+        }
+
+        
+    }
+}
---
+++ NUnitTestProject1/Integrationstest_Model_Controllere.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic_Layer;
+using Data_Layer;
+using Domain;
+using NSubstitute;
+
+namespace Test_ECG_Measure_System
+{
+    [TestFixture]
+    class Integrationstest_Model_Controllere
+    {
+        private Model _sut;
+        private Enter_Patient_Data_Controller _enter_Patient_Data_Controller;
+        private Measure_ECG_Controller _measure_ECG_Controller;
+        private IADC _adc;
+        private ICalculator _HRV;
+        private ICalculator _puls;
+        private R_peak _rPeak;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Model();
+            _enter_Patient_Data_Controller = new Enter_Patient_Data_Controller(_sut);
+            _adc = Substitute.For<IADC>();
+            _HRV = Substitute.For<ICalculator>();
+            _puls = Substitute.For<ICalculator>();
+            _rPeak = new R_peak();
+            _measure_ECG_Controller = new Measure_ECG_Controller(_sut, _adc, _HRV, _puls, _rPeak);
+
+        }
+
+        [Test]
+        public void Model_EnterPatientDataController_SavePatientData()
+        {
+            string cprNumber = "1234567890";
+            string name = "Jytte";
+            string address = "Nuuk";
+            _enter_Patient_Data_Controller.SavePatientData(cprNumber, name, address);
+            Assert.That(_sut._CPRNumber, Is.EqualTo(cprNumber));
+            Assert.That(_sut._Name, Is.EqualTo(name));
+            Assert.That(_sut._Address, Is.EqualTo(address));
+
+        }
+
+        [Test]      
+        public void Model_MeasureECGController_DeleteECG_number1()
+        {
+            int deleteNumber = 1;
+            _measure_ECG_Controller.Delete_ECG(deleteNumber);
+            Assert.That(_sut.ECGLeadValues1_1, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues1_2, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues1_3, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void Model_MeasureECGController_DeleteECG_number2()
+        {
+            int deleteNumber = 2;
+            _measure_ECG_Controller.Delete_ECG(deleteNumber);
+            Assert.That(_sut.ECGLeadValues2_1, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues2_2, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues2_3, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void Model_MeasureECGController_DeleteECG_number3()
+        {
+            int deleteNumber = 3;
+            _measure_ECG_Controller.Delete_ECG(deleteNumber);
+            Assert.That(_sut.ECGLeadValues3_1, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues3_2, Is.EqualTo(null));
+            Assert.That(_sut.ECGLeadValues3_3, Is.EqualTo(null));
+        }
+
+
+    }
+}
